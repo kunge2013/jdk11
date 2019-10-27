856e85cde0e7cac5a52435b2d425dd8af7e268ad
fk <fk@ubuntu.ubuntu-domain>
2019-10-27 11:42:03 +0800

    httpclientDemo

---
+++ jdk11/src/jdk11/jep/http321/HttpCient.java
@@ -0,0 +1,18 @@
+package jdk11.jep.http321;
+
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+
+public class HttpCient {
+	public static void main(String[] args) {
+		HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2)
+//	      .followRedirects(Redirect.SAME_PROTOCOL)
+	      .proxy(ProxySelector.of(new InetSocketAddress("www.baidu.com", 80)))
+	      .authenticator(Authenticator.getDefault())
+	      .build();
+	}
+}
