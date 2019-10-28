1bf1d607e27996b650da0a45374509a7270b8074
panzhangyi <panzhangyi@squrab.com>
2019-10-28 19:45:47 +0800

    collection add

---
+++ jdk11/src/collections/ListUtils.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class ListUtils {
+	public static void main(String[] args) {
+	}
+	
+	/**
+	 * 	不可变List集合
+	 * @param objects
+	 * @return
+	 */
+	public static <E> List<E> unmodifyList(E... objects) {
+		return List.of(objects);
+	}
+	
+	/**
+	 * 	不可变SET集合
+	 * @param objects
+	 * @return
+	 */
+	public static <E> Set<E> unmodifySet(E... objects) {
+		return Set.of(objects);
+	}
+	
+	/**
+	 * 	不可变集合
+	 * @param objects
+	 * @return
+	 */
+	public static <K,V> Map<K,V> unmodifyMap(Entry<? extends K, ? extends V>... objects) {
+		return Map.ofEntries(objects);
+	}
+}
